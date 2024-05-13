58d964857dba3527f1d825ce2134c32922f656fd
Hnin <hwutyee@gmail.com>
2024-05-13 21:58:37 +0800

    cicd

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-module.exports.firsthandler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "This is get function!",
-        },
-        null,
-        2
-      ),
-    };
-  };
-  
-  module.exports.secondhandler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "This is post function!",
-        },
-        null,
-        2
-      ),
-    };
-  };
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /', () => {
+  it('responds with "Hello, world!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, world!');
+  });
+});
